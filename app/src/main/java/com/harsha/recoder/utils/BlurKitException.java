cf44a97986a3faaccb13710ba5c78cbf5cc5b94d
Harsha <maramsaiharsha128@gmail.com>
2021-05-17 06:34:47 +0530

    Initial commit

---
+++ app/src/main/java/com/harsha/recoder/utils/BlurKitException.java
@@ -0,0 +1,15 @@
+package com.harsha.recoder.utils;
+
+/**
+ * The type Blur kit exception.
+ */
+public class BlurKitException extends Exception {
+	/**
+	 * Instantiates a new Blur kit exception.
+	 *
+	 * @param message the message
+	 */
+	public BlurKitException (String message) {
+		super (message);
+	}
+}
